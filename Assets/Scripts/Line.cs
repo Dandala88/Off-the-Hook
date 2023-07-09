a0b0b5215d9d99d7bec0c6bc20f59c03b7dc2e28
Dan <me@danielholcomb.com>
2023-07-09 13:32:20 -0400

    grace period

--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -11,8 +11,12 @@ public class Line : MonoBehaviour
     public Vector3 lineEntry;
     [HideInInspector]
     public Transform bait;
+    [SerializeField]
+    private float gracePeriod;
     
     private LineRenderer lineRenderer;
+
+    private float t;
 
     private void Awake()
     {
@@ -26,15 +30,17 @@ public class Line : MonoBehaviour
         {
             lineRenderer.SetPosition(0, bait.position);
             lineRenderer.SetPosition(1, lineEntry);
-            if (lineEntry.y - bait.position.y < caughtDistance)
+            if (lineEntry.y - bait.position.y < caughtDistance && t >= gracePeriod)
             {
                 GameManager.Instance.calories = 0;
                 FishController.caught = false;
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
+            t += Time.deltaTime;
         }
         else
         {
+            t = 0;
             lineRenderer.SetPosition(0, Vector3.zero);
             lineRenderer.SetPosition(1, Vector3.zero);
         }
