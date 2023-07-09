5402bf0b1c8905d69d4c758ec85d2ff69bf1aa1b
Dan <me@danielholcomb.com>
2023-07-09 05:29:54 -0400

    Add gamemanger singleton and calorie keeper

--- Assets/Scripts/FoodItem.cs
+++ Assets/Scripts/FoodItem.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class FoodItem : Collectible
 {
+    public int calories;
+
     protected override void Captured(FishController fish)
     {
         base.Captured(fish);
-        Debug.Log("EATME");
+        GameManager.Instance.calories += calories;
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,4 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [HideInInspector]
+    public int calories;
+
+    private static GameManager instance;
+
+    public static GameManager Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
 }
